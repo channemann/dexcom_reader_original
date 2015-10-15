4dc176e6ff98246f67e617900ffc12a1fa027161
Chris Hannemann <chris.hannemann@gmail.com>
2015-10-15 16:31:58 -0700

    Initial Commit

--- dexcom_reader/database_records.py
+++ dexcom_reader/database_records.py
@@ -90,7 +90,6 @@ class GenericXMLRecord(GenericTimestampedRecord):
 
 
 class InsertionRecord(GenericTimestampedRecord):
-  FIELDS = ['insertion_time', 'session_state']
   FORMAT = '<3IcH'
 
   @property
@@ -118,7 +117,6 @@ class Calibration(GenericTimestampedRecord):
 
 class MeterRecord(GenericTimestampedRecord):
   FORMAT = '<2IHIH'
-  FIELDS = ['meter_glucose', 'meter_time' ]
 
   @property
   def meter_glucose(self):
